b9ea1cbc5bb24801e4acf971f0a1697670fe9953
Alex Eiras <alexeiras@aspgems.com>
2009-12-18 12:28:39 +0100

    implemented some missing actions

--- app/controllers/member/archives_controller.rb
+++ app/controllers/member/archives_controller.rb
@@ -14,9 +14,52 @@ class Member::ArchivesController < Member::BaseController
 
   def create
     @archive = current_user.archives.build(params[:archive])
-    if @archive.save
-      flash[:ok] = t('archives.messages.created')
-      redirect_to member_archives_url
+    respond_to do |format|
+      if @archive.save
+        format.html do 
+          flash[:ok] = t('member.archives.messages.created')
+          redirect_to member_archives_url
+        end
+      else
+        format.html do
+          flash[:error] = t('member.archives.messages.error_on_create')
+          render :action => 'new'
+        end
+      end
+    end
+  end
+
+  def update
+    @archive = current_user.archives.find(params[:id])
+    respond_to do |format|
+      if @archive.update_attributes(params[:archive])
+        format.html do 
+          flash[:ok] = t('member.archives.messages.updated')
+          redirect_to member_archives_url
+        end
+      else
+        format.html do
+          flash[:error] = t('member.archives.messages.error_on_update')
+          render :action => 'edit'
+        end
+      end
+    end
+  end
+
+  def destroy
+    @archive = current_user.archives.find(params[:id])
+    respond_to do |format|
+      if @archive.destroy
+        format.html do
+          flash[:ok] = t('member.archives.messages.deleted')
+          redirect_to member_archives_url
+        end
+      else
+        format.html do
+          flash[:error] = t('member.archives.messages.error_on_delete')
+          redirect_to member_archives_url
+        end
+      end
     end
   end
 
--- app/controllers/member/uploads_controller.rb
+++ app/controllers/member/uploads_controller.rb
@@ -2,40 +2,67 @@ class Member::UploadsController < Member::BaseController
 
   before_filter :find_archive
 
-  def new
-    @upload = @archive.uploads.new
+  def index
+    @uploads = @archive.uploads.paginate(pagination_options)
   end
 
-  def create
-    @upload = @archive.uploads.build(params[:upload])
-    if @upload.save
-      respond_to do |format|
-        format.html do
-          flash[:ok] = t('uploads.messages.created')
-          redirect_to member_archive_uploads_url(@archive)
-        end
-      end
-    end
+  def new
+    @upload = @archive.uploads.new
   end
 
   def edit
     @upload = @archive.uploads.find(params[:id])
   end
 
-  def destroy
-    @upload = @archive.uploads.find(params[:id])
-    if @upload.destroy
-      respond_to do |format|
+  def create
+    @upload = @archive.uploads.build(params[:upload])
+    respond_to do |format|
+      if @upload.save
         format.html do
-          flash[:ok] = t('uploads.messages.deleted')
+          flash[:ok] = t('member.uploads.messages.created')
           redirect_to member_archive_uploads_url(@archive)
+        end
+      else
+        format.html do
+          flash[:error] = t('member.uploads.messages.error_on_create')
+          render :action => 'new'
         end
       end
     end
   end
 
-  def index
-    @uploads = @archive.uploads.paginate(pagination_options)
+  def update
+    @upload = @archive.uploads.find(params[:id])
+    respond_to do |format|
+      if @upload.update_attributes(params[:upload])
+        format.html do
+          flash[:ok] = t('member.uploads.messages.updated')
+          redirect_to member_archive_uploads_url(@archive)
+        end
+      else
+        format.html do
+          flash[:error] = t('member.uploads.messages.error_on_update')
+          render :action => 'edit'
+        end
+      end
+    end
+  end
+
+  def destroy
+    @upload = @archive.uploads.find(params[:id])
+    respond_to do |format|
+      if @upload.destroy
+        format.html do
+          flash[:ok] = t('member.uploads.messages.deleted')
+          redirect_to member_archive_uploads_url(@archive)
+        end
+      else
+        format.html do
+          flash[:error] = t('member.uploads.messages.error_on_delete')
+          redirect_to member_archive_uploads_url(@archive)
+        end
+      end
+    end
   end
 
   private
